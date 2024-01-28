93377ed570e149a023240152df9ec40a007552e7
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-28 18:40:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn import datasets
 
 st.write("""
 # Visualización de datos de la base de datos Iris
