6f3ec51818707b6a788375ec2832a5eba22afca4
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-28 18:03:04 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.write("""
+# Visualización de datos de la base de datos **Iris**
+
+Flores Iris
+""")
+
+iris = datasets.load_iris()
+iris_df = pd.DataFrame(data = iris['data'], columns=iris['feature_names'])
+iris_df['Iris name'] = iris_df['Iris type'].apply(lambda x: 'setosa' if x==0 else ('versicolor' if x==1 else 'virginica'))
+
+primeros = iris_df.head()
+st.dataframe(primeros)
