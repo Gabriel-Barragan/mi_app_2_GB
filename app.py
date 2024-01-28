3992ccecb7e25733894c069b3510a65d544011a1
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-28 18:48:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ from sklearn import datasets
 st.write("""
 # Visualización de datos de la base de datos Iris
 
-Flores Iris
 """)
 
-st.sidebar.header('Visualización')
+st.sidebar.header('Autor: Gabriel Barragán')
 
 iris = datasets.load_iris()
 iris_df = pd.DataFrame(data = iris['data'], columns=iris['feature_names'])
 iris_df['Iris type'] = iris['target']
 iris_df['Iris name'] = iris_df['Iris type'].apply(lambda x: 'setosa' if x==0 else ('versicolor' if x==1 else 'virginica'))
 
+st.write('Primeros 5 datos')
 primeros = iris_df.head()
 st.write(primeros)
+
+st.write('Últimos 5 datos')
+ultimos = iris_df.tail()
+st.write(ultimos)
