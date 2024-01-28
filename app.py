acd853220a6aa353407c273f8bc1c373fc8ee9b8
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-28 18:52:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,7 @@ st.write(primeros)
 st.write('Últimos 5 datos')
 ultimos = iris_df.tail()
 st.write(ultimos)
+
+st.write(Visualización)
+plot = sns.pairplot(data = iris_df[iris_df.columns.difference(['Iris type'])], hue = 'Iris name', palette='Set2')
+st.pyplot(plot.fig)
