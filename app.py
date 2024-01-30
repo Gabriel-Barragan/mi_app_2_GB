f21d0fa602c686dce1759ddd169eff15a84645e4
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-01-29 19:31:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,6 @@ st.write(ultimos)
 st.write('Visualización')
 plot_1 = sns.pairplot(data = iris_df[iris_df.columns.difference(['Iris type'])], hue = 'Iris name', palette='Set2')
 st.pyplot(plot_1.fig)
+
+plot_2 = sns.heatmap(iris_df[iris_df.columns.difference(['Iris type'])].corr(), cmap='icefire')
+st.pyplot(plot_2.fig)
